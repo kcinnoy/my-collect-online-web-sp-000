0e53814107f5b2a238d5480bebfeb462a8ed78ba
Yonnick Brown <25hour24@gmail.com>
2019-01-28 21:52:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   i = 0
   new_array =[]
   while i < array.length
-    new_array << yield (array[i]).upcase
+    new_array << yield (array[i].upcase)
     i +=1
   end
   else
