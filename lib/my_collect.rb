22eb7968898a5916193f16a16363909078f00cd6
Yonnick Brown <25hour24@gmail.com>
2019-01-28 21:44:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  if block_given?
+  i = 0
+  new_array =[]
+  while i < array.length
+    yield do |e|
+      new_array << e.upcase
+    end
+  end 
+  else
+    puts "Hey! No block was given!"
+  end
+  new_array
+end
